3fb1f4a3c74bde6154db07d38736817c64e34069
Pavel Tarasau <zefirfricadelkovich@gmail.com>
2024-07-02 23:38:22 +0300

    first commit

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -2,7 +2,15 @@ package com.epam.langSyntax;
 
 public class NumberReverter {
 
+    int reverse = 0;
     public void revert(int number) {
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+        }
     }
 
 }
+
